86c6e6d9df03653c0000c0c8d3ed6b4865747448
nik-pakhomov <nikpackhomov@gmail.com>
2020-10-07 21:40:33 +0300

    Changes after video 181 of React - The Complete Udemy Course

--- React - The Complete Guide/http-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React - The Complete Guide/http-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = { salad: 0.5, cheese: 0.4, meat: 1.3, bacon: 0.7 };
 
@@ -70,7 +71,27 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    // alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Nikita Pakhomov",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "41351",
+          country: "Russia",
+        },
+        email: "test@test.com",
+      },
+      delivaryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
   };
 
   render() {
