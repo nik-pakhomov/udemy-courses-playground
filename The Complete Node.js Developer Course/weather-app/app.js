c1f673ce97b919041243d36723c2fe006d264191
nik-pakhomov <nikpackhomov@gmail.com>
2020-12-17 00:26:08 +0300

    Changes after video 32 of The Complete Node.js Developer Course

--- The Complete Node.js Developer Course/weather-app/app.js
+++ The Complete Node.js Developer Course/weather-app/app.js
@@ -1,9 +1,15 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=3b88940dfbdb3c79a7a9b0ce3b85ce4c&query=Saint-Petersburg";
+  "http://api.weatherstack.com/current?access_key=3b88940dfbdb3c79a7a9b0ce3b85ce4c&query=Saint-Petersburg&units=f";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It's currently " +
+      response.body.current.temperature +
+      " degrees out. It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  );
 });
