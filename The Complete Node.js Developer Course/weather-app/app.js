e3a55c2d167d87c5ea2c458d8ea774326064ca24
nik-pakhomov <nikpackhomov@gmail.com>
2021-01-09 22:05:51 +0300

    Changes after video 34 of The Complete Node.js Developer Course

--- The Complete Node.js Developer Course/weather-app/app.js
+++ The Complete Node.js Developer Course/weather-app/app.js
@@ -1,25 +1,37 @@
 const request = require("request");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=3b88940dfbdb3c79a7a9b0ce3b85ce4c&query=Saint-Petersburg&units=f";
+// const url =
+//   "http://api.weatherstack.com/current?access_key=3b88940dfbdb3c79a7a9b0ce3b85ce4c&query=Saint-Petersburg&units=f";
 
-request({ url: url, json: true }, (error, response) => {
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It's currently " +
-      response.body.current.temperature +
-      " degrees out. It feels like " +
-      response.body.current.feelslike +
-      " degrees out."
-  );
-});
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     console.log(
+//       response.body.current.weather_descriptions[0] +
+//         ". It's currently " +
+//         response.body.current.temperature +
+//         " degrees out. It feels like " +
+//         response.body.current.feelslike +
+//         " degrees out."
+//     );
+//   }
+// });
 
 const geocodeURL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibnBha2hvbW92IiwiYSI6ImNraXJ4cGdqdjBmZG8ycXNjajM3MXc2MHQifQ.9mWZSlUZlR_vXi5wyJtHlw&limit=1";
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
 
-  console.log(latitude, longitude);
+    console.log(latitude, longitude);
+  }
 });
