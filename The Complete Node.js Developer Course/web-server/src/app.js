cd40a544b56cf0d7295f7b8044b76d9ae90ced41
nik-pakhomov <nikpackhomov@gmail.com>
2021-02-08 15:06:12 +0300

    Changes after video 49 of The Complete Node.js Developer Course

--- The Complete Node.js Developer Course/web-server/src/app.js
+++ The Complete Node.js Developer Course/web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -23,7 +26,11 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { helpText: "Help is on the way!" });
+  res.render("help", {
+    helpText: "Help is on the way!",
+    title: "Help",
+    name: "Nikita Pakhomov",
+  });
 });
 
 app.get("/weather", (req, res) => {
